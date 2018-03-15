1abdf23372875893149fe6af9f83733e9d7f33d6
Irene Florez <gummyrene@gmail.com>
2018-03-15 10:40:00 -0700

    NYT api script setup

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ var myLocations = [
   {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
 ];
 
+
 // setting up viewmodel
 function mapViewModel() {
 var self = this;
@@ -26,6 +27,38 @@ var self = this;
             infowindow.open(map, marker);
             infowindow.addListener('closeclick', function() {
                 infowindow.marker = null;
+
+
+                var location = marker.getLat() + ',' + marker.getLng();
+                var nytkey = '16c51504fabd43868ccf75a7028ac754'
+                var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + 
+                                '&sort=newest&api-key=' +
+                                nytkey;
+                
+                $.getJSON(nytimesUrl, function(data){
+                    
+                    var articles  = data.response.docs;
+                    var content = '<p><b>' + marker.getTitle() +
+                    '</b></p><ul>';
+                    for (var i = 0; i < articles.length; i++) {
+                        content += '<li><a href="' + articles[i].web_url +
+                         '" target="_blank">NYT-Article ' +
+                          (i + 1) +
+                         '</a></li>';
+                    }
+                    if (!(articles.length)) {
+                        content += '<li>No Articles Found</li>';
+                    }
+                    content += '</ul>';
+                    infowindow.setContent(content);
+                }).error(function() {
+                    alert("There was an issue loading the NYT-Article API.");
+                });
+                infowindow.open(map, marker);
+
+
+
+
             });
         }
     };
@@ -117,3 +150,74 @@ errorHandling = function errorHandling() {
 function startApp() {
     ko.applyBindings(new mapViewModel());
 }
+
+// function loadData() {
+//     var $body = $('body');
+//     var $wikiElem = $('#wikipedia-links');
+//     var $nytHeaderElem = $('#nytimes-header');
+//     var $nytElem = $('#nytimes-articles');
+//     //var $greeting = $('#greeting');
+
+//     // clear out old data before new request
+//     $wikiElem.text("");
+//     $nytElem.text("");
+
+//     //var streetStr = $('#street').val();
+//     //var cityStr = $('#city').val();
+//     var location = marker.lat + ',' + marker.lng;
+//     var gmapskey = 'AIzaSyDA6qoUKQ3WJQd4ZxAahhFPM80WJbw-yws';
+
+//     // load streetview
+//     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + location + '&heading=151.78&pitch=-0.76&key=' + gmapskey;
+
+//     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+            
+//     // load nytimes
+//             var nytkey = '16c51504fabd43868ccf75a7028ac754'
+//             var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + 
+//                 '&sort=newest&api-key=' +
+//                 nytkey;
+//             $.getJSON(nytimesUrl, function(data){
+                
+//                 articles = data.response.docs;
+//                 for (var i = 0; i < articles.length; i++) {
+//                     var article = articles[i];
+//                     $nytElem.append('<li class="article">'+
+//                         '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+//                         '<p>' + article.snippet + '</p>'+
+//                     '</li>');
+//                 };
+
+//                 //infowindow.setContent(this.htmlContent + this.nytApiContent);
+//             }).error(function() {
+//                 alert("There was an issue loading the NYT-Article API.");
+//             });
+
+
+//     // load wikipedia data
+//     var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + location + '&format=json&callback=wikiCallback';
+//     var wikiRequestTimeout = setTimeout(function(){
+//         $wikiElem.text("failed to get wikipedia resources");
+//     }, 8000);
+
+//     $.ajax({
+//         url: wikiUrl,
+//         dataType: "jsonp",
+//         jsonp: "callback",
+//         success: function( response ) {
+//             var articleList = response[1];
+
+//             for (var i = 0; i < articleList.length; i++) {
+//                 articleStr = articleList[i];
+//                 var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+//                 $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+//             };
+
+//             clearTimeout(wikiRequestTimeout);
+//         }
+//     });
+
+//     return false;
+// };
+
+// $('#form-container').submit(loadData);
